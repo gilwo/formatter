c5bb79ed322dc093f85f868919bf0886751a82d3
Gil <gilwo@null.net>
2017-10-28 20:23:12 +0300

    add example, ehance the README

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # formatter
 formatter of text when printing consecutive data which need to be at the same format
+
+usage example
+```go
+import "github.com/gilwo/formatter"
+```
+
+need to set fields
+```go
+    fields := []formatter.LineFormatterField{
+                {15, formatter.AlignDirCenter},
+                {7,  formatter.AlignDirLeft},
+                {10, formatter.AlignDirRight},
+                {9,  formatter.AlignDirNone},   // <- no alignment, just limit the field length
+                {0,  formatter.AlignDirCenter}, // <- field of len 0 override any alignment
+            }
+```
+
+define format string
+```go
+    formatString := "< %s | %s | %s | %s | %s -->\n"
+```
+
+now get the closure function 
+```go
+    lineFunc := formatter.LineFormatter(formatString, fields...)
+```
+
+now just print the values
+```go
+    fmt.Printf(lineFunc("A", "B", "C", "D", "D" ))
+    fmt.Printf(lineFunc("Coloumn A", "Column B", "Column C", "Column D", "Column E" ))
+    fmt.Printf(lineFunc("mama", 444, "let", "me and you go to where we want", 99191))
+    fmt.Printf(lineFunc("mama", struct{x string; y int}{"why so serious", 777}, "let", "me", 111))
+    fmt.Printf(lineFunc("mama", 1234567890, "let", "fantastic mr. fox", map[string]struct{x string; y int}{"key": {"val", 5}}))
+```
+
+the outout will look like this
+```bash
+<        A        | B       |          C | D | D -->
+<    Coloumn A    | Colu... |   Column C | Column D | Column E -->
+<       mama      | 444     |        let | me and... | 99191 -->
+<       mama      | {why... |        let | me | 111 -->
+<       mama      | 1234... |        let | fantas... | map[key:{val 5}] -->
+```
--- example/example.go
+++ example/example.go
@@ -9,12 +9,12 @@ import (
 
 func main() {
 
-    f := line1.LineFormatter("<-- %s | %s | %s : %s -->\n",
-        []line1.LineFormatterField{
-            {20, line1.AlignDirCenter},
-            {20, line1.AlignDirCenter},
-            {20, line1.AlignDirCenter},
-            {20, line1.AlignDirCenter},
+    f := formatter.LineFormatter("<-- %s | %s | %s : %s -->\n",
+        []formatter.LineFormatterField{
+            {20, formatter.AlignDirCenter},
+            {20, formatter.AlignDirCenter},
+            {20, formatter.AlignDirCenter},
+            {20, formatter.AlignDirCenter},
         }...)
 
     fmt.Printf(f("a", "b", "c", "d"))
@@ -22,16 +22,32 @@ func main() {
     fmt.Printf(f("mama", struct{x int; y string}{545345, "ab hfhf hf kjdshkjdh ksjdkjd dkfjh bb"}, "let", "me"))
 
     fmt.Println()
-    f2 := line1.LineFormatter("<-- %s | %s | %s : %s :: '%s' -->\n",
-        []line1.LineFormatterField{
-            {4, line1.AlignDirCenter},
-            {20, line1.AlignDirCenter},
-            {20, line1.AlignDirCenter},
-            {20, line1.AlignDirNone},
-            {0, line1.AlignDirNone},
+    f2 := formatter.LineFormatter("<-- %s | %s | %s : %s :: '%s' -->\n",
+        []formatter.LineFormatterField{
+            {4, formatter.AlignDirCenter},
+            {20, formatter.AlignDirCenter},
+            {20, formatter.AlignDirCenter},
+            {20, formatter.AlignDirNone},
+            {0, formatter.AlignDirNone},
         }...)
 
     fmt.Printf(f2("title a", "title b", "title c", "title d", "title e"))
     fmt.Printf(f2("mama", 444, "let", "me", "ending value for first line"))
     fmt.Printf(f2("mama", "let", "me", struct{x int; y string}{545322145, "a very long string to represnet string with length that is not limimted by some size and using align none show everythong ... "}, "bit"))
+
+
+    fields := []formatter.LineFormatterField{
+                {15, formatter.AlignDirCenter},
+                {7,  formatter.AlignDirLeft},
+                {10, formatter.AlignDirRight},
+                {9,  formatter.AlignDirNone},   // <- no alignment, just limit the field length
+                {0,  formatter.AlignDirCenter}, // <- field of len 0 override any alignment
+            }
+    lineFunc := formatter.LineFormatter("< %s | %s | %s | %s | %s -->\n", fields...)
+
+    fmt.Printf(lineFunc("A", "B", "C", "D", "D" ))
+    fmt.Printf(lineFunc("Coloumn A", "Column B", "Column C", "Column D", "Column E" ))
+    fmt.Printf(lineFunc("mama", 444, "let", "me and you go to where we want", 99191))
+    fmt.Printf(lineFunc("mama", struct{x string; y int}{"why so serious", 777}, "let", "me", 111))
+    fmt.Printf(lineFunc("mama", 1234567890, "let", "fantastic mr. fox", map[string]struct{x string; y int}{"key": {"val", 5}}))
 }
